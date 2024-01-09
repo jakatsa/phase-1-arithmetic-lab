60b85abf9d1f22eaa7e4eda4f423a1adb69c1625
Joe <jakatsa5@gmail.com>
2024-01-09 12:18:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
 // Write your code here
+let num1 = 1;
+let num2 = 62;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 8;
+let num4 = 2;
+let mod = num3 / num4;
+let max = Math.max(10, 9, 6, 4, 3, 7, 20);
